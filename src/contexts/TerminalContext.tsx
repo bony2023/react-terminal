96b75d97bf1ecc1ffd971c47d2853053663f1ce1
Bony Roopchandani <bonyroopchandani@gmail.com>
2020-04-12 21:08:44 +0530

    fix history navigation  On branch develop 	modified:   src/contexts/TerminalContext.tsx

--- src/contexts/TerminalContext.tsx
+++ src/contexts/TerminalContext.tsx
@@ -6,23 +6,30 @@ export const TerminalContextProvider = (props: any) => {
   const { children } = props;
   const [bufferedContent, setBufferedContent] = React.useState("");
   const [commandsHistory, setCommandsHistory] = React.useState([]);
-  const [historyPointer, setHistoryPointer] = React.useState(-1);
+  const [historyPointer, setHistoryPointer] = React.useState(null);
+
+  React.useEffect(() => {
+    setHistoryPointer(commandsHistory.length);
+  }, [commandsHistory]);
 
   const appendCommandToHistory = (command: string) => {
     if (!command) {
       return;
     }
 
-    setHistoryPointer(commandsHistory.length);
     setCommandsHistory(commandsHistory.concat(command));
   };
 
   const getPreviousCommand = () => {
-    if (historyPointer === -1) {
-      return "";
+    if (historyPointer === 0) {
+      if (commandsHistory.length === 0) {
+        return "";
+      }
+
+      return commandsHistory[0];
     }
 
-    const command = commandsHistory[historyPointer];
+    const command = commandsHistory[historyPointer - 1];
     if (historyPointer > 0) {
       setHistoryPointer(historyPointer - 1);
     }
@@ -31,13 +38,16 @@ export const TerminalContextProvider = (props: any) => {
   };
 
   const getNextCommand = () => {
-    if (historyPointer + 1 === commandsHistory.length) {
-      return "";
+    if (historyPointer + 1 <= commandsHistory.length) {
+      if (historyPointer === commandsHistory.length) {
+        return "";
+      }
+      const command = commandsHistory[historyPointer + 1];
+      setHistoryPointer(historyPointer + 1);
+      return command;
     }
 
-    const command = commandsHistory[historyPointer + 1];
-    setHistoryPointer(historyPointer + 1);
-    return command;
+    return "";
   };
 
   return (
