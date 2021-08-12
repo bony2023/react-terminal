5f6f1c2316fa774f053b7263881af1044112131a
benmneb <62878664+benmneb@users.noreply.github.com>
2021-08-13 02:23:44 +0800

    Add `PropTypes.object` to `errorMessage`

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -69,7 +69,7 @@ Terminal.propTypes = {
     PropTypes.node
   ])),
   welcomeMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.node]),
-  errorMessage: PropTypes.string
+  errorMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
 };
 
 Terminal.defaultProps = {
