4e4ecd5ec8aa81023caf6b9169261f2501759a6a
Bony Roopchandani <bony.roopchandani@shopify.com>
2023-11-09 20:06:14 +0530

    add newline to non-empty command

--- src/hooks/editor.tsx
+++ src/hooks/editor.tsx
@@ -180,6 +180,7 @@ export const useBufferedContent = (
             {output ? (
               <span>
                 {output}
+                <br/>
               </span>
             ) : null}
           </>
